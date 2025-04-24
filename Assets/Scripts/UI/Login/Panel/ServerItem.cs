f69d10d821324221f92f2d1a83d42d5d634a9897
isolation <23796978@qq.com>
2025-04-24 19:34:56 +0800

    feat: 完成选服面板拼接

---
+++ Assets/Scripts/UI/Login/Panel/ServerItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerItem : MonoBehaviour
+{
+    public UIButton btn;
+    public UILabel labInfo;
+
+    private int _beginIndex;
+    private int _endIndex;
+
+    void Start()
+    {
+        btn.onClick.Add(new EventDelegate(() => { }));
+    }
+
+    public void InitInfo(int beginIndex, int endIndex)
+    {
+        _beginIndex = beginIndex;
+        _endIndex = endIndex;
+
+        labInfo.text = $"{_beginIndex} - {_endIndex}区";
+    }
+}
